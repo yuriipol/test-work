307994fc3f6340fa8e845d9af9095f393a59a23a
yuriipol <104125040+yuriipol@users.noreply.github.com>
2023-04-13 14:51:32 +0300

    add style

--- src/Components/Pages/Users/Users.jsx
+++ src/Components/Pages/Users/Users.jsx
@@ -2,6 +2,9 @@ import style from "./Users.module.css";
 import { useState, useEffect } from "react";
 import { getUsersCards } from "../../../Servises/tweetsApi";
 import logo from "../../../images/Logo.png";
+import fonPictire from "../../../images/picture.png";
+import linePicture from "../../../images/Line.png";
+import ellipse from "../../../images/Boy.png";
 
 const Users = () => {
   const [users, setUsers] = useState([]);
@@ -14,16 +17,27 @@ const Users = () => {
 
     usersCards();
   }, []);
-  //   console.log(users);
-  const userItem = users.map(({ id, user, followers, tweeets, avatar }) => (
+  // console.log(users);
+  const userItem = users.map(({ id, user, followers, tweets, avatar }) => (
     <li key={id} className={style.item}>
-      <img className={style.imageLogo} src={logo} alt="Logo" />
+      <img className={style.imageLogo} src={logo} alt="logo" />
+      <img className={style.fonPicture} src={fonPictire} alt="fon" />
+
+      <img className={style.linePicture} src={linePicture} alt="line" />
+
+      <img className={style.ellipse} src={ellipse} alt="ellipse" />
+
       <img className={style.image} src={avatar} alt={user} />
+      <p className={style.tweets}>{tweets} tweets</p>
+      <p className={style.followers}>{followers} followers</p>
+      <button type="button" className={style.button}>
+        Follow
+      </button>
     </li>
   ));
   return (
     <>
-      <ul className={style.UserList}>{userItem}</ul>
+      <ul className={style.userList}>{userItem}</ul>
     </>
   );
 };
