eb81e8ca96b34fbcfdd045b9a9859cc351e4c2a8
yuriipol <104125040+yuriipol@users.noreply.github.com>
2023-04-18 10:01:09 +0300

    add style

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -1,9 +1,13 @@
+import { NavLink } from "react-router-dom";
 import style from "./Home.module.css";
 
 const Home = () => {
   return (
-    <div>
+    <div className={style.home}>
       <h2 className={style.title}>Page HOME</h2>
+      <NavLink to={"/tweets"} className={style.tweets}>
+        <span className={style.textButton}>Go to tweets</span>
+      </NavLink>
     </div>
   );
 };
